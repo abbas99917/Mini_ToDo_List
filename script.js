09c7766a152a20c7da5a3da94ebd7f4ba77dda08
abbas99917 <abbasgraphic17@gmail.com>
2026-02-17 23:12:43 -0800

    add localStorage

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 
-
     let data = document.getElementById("inputData");
     let btn = document.getElementById("addText");
     let ul = document.querySelector(".list");
 
-    btn.addEventListener("click",()=>{
+let items = JSON.parse(localStorage.getItem("todolist" || []));
+
+function displaList(){
+    ul.innerHTML = "";
+    items.forEach((item, index )=>{
         let li = document.createElement("li");
-        li.innerText = data.value;
+        li.innerText = item;
         let delbtn = document.createElement("button");
         delbtn.innerText = "Delete";
+        delbtn.addEventListener("click",()=>{
+            items.splice(index, 1);
+            localStorage.setItem("todolist",JSON.stringify(items));
+            displaList();
+        });
         ul.appendChild(li);
         li.appendChild(delbtn);
-        delbtn.addEventListener("click",()=>{
-            ul.removeChild(li);
-        })
-        data.value = "";
     });
+};
+displaList();
+
+btn.addEventListener("click", ()=>{
+    if(data.value.trim()=== "") return;
+    items.push(data.value);
+    localStorage.setItem("todolist",JSON.stringify(items));
+    displaList();
+    data.value = "";
+})
+    // btn.addEventListener("click",()=>{
+    //     if(data.value.trim() === "")return;
+    //     let li = document.createElement("li");
+    //     li.innerText = data.value;
+    //     let delbtn = document.createElement("button");
+    //     delbtn.innerText = "Delete";
+    //     ul.appendChild(li);
+    //     li.appendChild(delbtn);
+    //     delbtn.addEventListener("click",()=>{
+    //         ul.removeChild(li);
+    //     })
+    //     data.value = "";
+    // });
